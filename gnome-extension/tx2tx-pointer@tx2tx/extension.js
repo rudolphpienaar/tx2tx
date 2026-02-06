79de219115d88301d37994919aebb48b530dc457
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2026-02-06 18:34:44 -0500

    fix: make GNOME extension loader-compatible

--- gnome-extension/tx2tx-pointer@tx2tx/extension.js
+++ gnome-extension/tx2tx-pointer@tx2tx/extension.js
@@ -1,7 +1,4 @@
-/* exported init enable disable */
-
-const { Gio } = imports.gi;
-const Main = imports.ui.main;
+const Gio = imports.gi.Gio;
 
 const DBUS_XML = `
 <node>
@@ -13,32 +10,43 @@ const DBUS_XML = `
   </interface>
 </node>`;
 
-let _dbusImpl = null;
-let _dbusExport = null;
-
-const PointerProvider = class {
-  GetPointer() {
-    const [x, y] = global.get_pointer();
-    return [x, y];
-  }
-};
-
-function init() {}
-
-function enable() {
-  if (_dbusExport) {
-    return;
-  }
-
-  _dbusImpl = Gio.DBusExportedObject.wrapJSObject(DBUS_XML, new PointerProvider());
-  _dbusImpl.export(Gio.DBus.session, '/org/tx2tx/Pointer');
-  _dbusExport = _dbusImpl;
+class PointerProvider {
+    GetPointer() {
+        const [x, y] = global.get_pointer();
+        return [x, y];
+    }
 }
 
-function disable() {
-  if (_dbusExport) {
-    _dbusExport.unexport();
-    _dbusExport = null;
-  }
-  _dbusImpl = null;
+class Tx2txPointerExtension {
+    enable() {
+        if (this._dbusExport)
+            return;
+
+        this._nameId = Gio.bus_own_name(
+            Gio.BusType.SESSION,
+            'org.tx2tx.Pointer',
+            Gio.BusNameOwnerFlags.NONE,
+            null,
+            null,
+            null
+        );
+
+        this._dbusExport = Gio.DBusExportedObject.wrapJSObject(DBUS_XML, new PointerProvider());
+        this._dbusExport.export(Gio.DBus.session, '/org/tx2tx/Pointer');
+    }
+
+    disable() {
+        if (this._dbusExport) {
+            this._dbusExport.unexport();
+            this._dbusExport = null;
+        }
+        if (this._nameId) {
+            Gio.bus_unown_name(this._nameId);
+            this._nameId = 0;
+        }
+    }
+}
+
+function init() {
+    return new Tx2txPointerExtension();
 }
